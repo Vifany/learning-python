c527443f15ddc4486d79f48a9b9f9e4689351e94
Vifany <bos9lolkin@gmail.com>
2022-09-11 02:05:52 +0300

    inital commit

---
+++ zadachnik/factorisation.py
@@ -0,0 +1,35 @@
+"""Разлагает число на простые множители"""
+
+import math
+import copy
+import sympy
+
+
+
+def factorize(tar):
+    """Факторизует целое число"""
+    num = copy.deepcopy(tar)
+    prime_lst = [int(x) for x in sympy.primerange(2, int(math.sqrt(num)+1))]
+    for prime in reversed(prime_lst):
+        while num%prime == 0:
+            num = int(num/prime)
+            yield prime
+            if sympy.isprime(num):
+                yield num
+                break
+        else: continue
+        break
+
+
+def main():
+    """Основная функция"""
+    tar = int(input('Введите целевое число: '))
+    print(f'Число {tar} факторизуется как: ', end='')
+    factorizer = factorize(tar)
+    print(f'{next(factorizer)} ', end ='')
+    for item in factorizer:
+        print(f'* {item} ', end ='')
+
+
+if __name__ == '__main__':
+    main()
