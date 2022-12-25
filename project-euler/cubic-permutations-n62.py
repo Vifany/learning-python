173aa9981775b1a28ce4b641ba4e574bc8278c45
Vifany <bos9lolkin@gmail.com>
2022-12-25 05:54:55 +0300

    refactor

--- project-euler/cubic-permutations-n62.py
+++ project-euler/cubic-permutations-n62.py
@@ -1,10 +1,10 @@
 import itertools
 import string
 def decompose_int(val):
-    '''Decomposes int into list with count of used digits'''
-    return [str(val).count(i) for i in string.digits]
+    '''Decomposes int into tuple with count of used digits'''
+    return tuple([str(val).count(i) for i in string.digits])
 
-def searchlist(amount, power):
+def searchlist(amount: int, power: int):
     '''
     Searches lowest base of exponent with set amount
     of permutations which have integer root of same power
@@ -16,4 +16,5 @@ def searchlist(amount, power):
         if retlist.count(retlist[-1]) == amount:
             return pow(retlist.index(retlist[-1], 0), power)
 
-print(searchlist(5, 3))
+if __name__ == '__main__':
+    print(searchlist(5, 3))
